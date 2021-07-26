74b2f4f5b17cabeff0109f6244f89648876d67d4
Satyam175 <61234615+Satyam175@users.noreply.github.com>
2021-07-26 12:21:03 +0530

    Updates

---
+++ precache-manifest.b71d0d8836132baf9a3caff0f234f807.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777d5ef815034f437841dec4492a32cf",
+    "url": "/myport2/index.html"
+  },
+  {
+    "revision": "e8395f858cc6c154e460",
+    "url": "/myport2/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "926578480bbe09182c5d",
+    "url": "/myport2/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "60bd7766074baa8015cb",
+    "url": "/myport2/static/css/main.fd8e1b7c.chunk.css"
+  },
+  {
+    "revision": "e4e604039335e29db7bf",
+    "url": "/myport2/static/js/2.d98db984.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/myport2/static/js/2.d98db984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8395f858cc6c154e460",
+    "url": "/myport2/static/js/3.c19083fb.chunk.js"
+  },
+  {
+    "revision": "926578480bbe09182c5d",
+    "url": "/myport2/static/js/4.db1fe7c1.chunk.js"
+  },
+  {
+    "revision": "60bd7766074baa8015cb",
+    "url": "/myport2/static/js/main.39edf40e.chunk.js"
+  },
+  {
+    "revision": "7690746bc0dc5d3c8792",
+    "url": "/myport2/static/js/runtime-main.4af826cb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/myport2/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/myport2/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "17e0329ec6060f3cfbb97e762cecb833",
+    "url": "/myport2/static/media/baba.17e0329e.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/myport2/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/myport2/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d4cd31abcfdd3175bc0cb4f16be991f3",
+    "url": "/myport2/static/media/jecLogo.d4cd31ab.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/myport2/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/myport2/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/myport2/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/myport2/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/myport2/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
